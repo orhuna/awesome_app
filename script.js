add8021b438cec3341486a9f1c9fbae534c38091
Orhun Aydin <janorhuna@gmail.com>
2023-01-18 17:55:43 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+var mymap = L.map('mapid').setView([39.97080543542897, -105.11993408203126], 18);
+//https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
+//'http://a.tile.stamen.com/toner/{z}/{x}/{y}.png'
+//https://wiki.openstreetmap.org/wiki/Tile_servers
+L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map tiles by Stamen Design, under CC BY 3.0.',
+        maxZoom: 18
+}).addTo(mymap);
+
+var marker = L.marker([39.97080543542897, -105.11993408203126]).addTo(mymap);
+
+var circle = L.circle([39.97080543542897, -105.11993408203126], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
+
+var polygon2 = L.polygon([
+    [55.509, -0.01],
+    [55.503, -0.02],
+    [55.51, -0.47]
+]).addTo(mymap);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+polygon.bindPopup("I am another polygon.");
